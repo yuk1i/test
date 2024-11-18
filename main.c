23f66c7ebf3c377408008a823842acb5d546da73
Yuki <chenjf@mail.sustech.edu.cn>
2024-11-18 15:26:26 +0800

    4

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
+int call(int arg0, int arg1);
+
 int main() {
     int b = 0;
     int c = b+1;
     int a = call(b,c);
     int d = a;
     d = d + 1;
-    return d;
+    return 0;
 }
 
 int call(int arg0, int arg1) {
