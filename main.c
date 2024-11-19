d81c8e2c21bfe827cc9498090a69b51b50ffe8c7
Yuki <chenjf@mail.sustech.edu.cn>
2024-11-19 18:39:34 +0800

    add conditional

--- main.c
+++ main.c
@@ -5,11 +5,17 @@ int main() {
     int c = b+1;
     int a = call(b,c);
     int d = a;
+    if (d > 0) {
+        d = -d;
+    }
     d = d + 1;
     return 0;
 }
 
 int call(int arg0, int arg1) {
+    if (arg0 < 999) {
+        arg1 += 1919810;
+    }
     arg0 += arg1;
     return arg0 + 114514;
 }
