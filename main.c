b7d97afec4b4081c002feb9c6c097a8798d3eb3f
Yuki <chenjf2020@mail.sustech.edu.cn>
2025-09-10 19:27:51 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,8 @@
-#include <iostream>
-
-class MyClass {
-public:
-    MyClass(int value): value(value) {}
-    int value;
-};
-
-void useObject(MyClass* obj) {
-    std::cout << "Using object with value: " << obj->value << std::endl;
-}
-
-void deleteObject(MyClass* obj) {
-    delete obj; // 释放内存
-}
-
-int main() {
-    MyClass* myObj = new MyClass(42); // 创建对象
-    useObject(myObj);                // 使用对象
-    deleteObject(myObj);             // 删除对象，释放内存
-    useObject(myObj);                // 使用对象2
-    return 0;
+int test1(int i) {
+    int b = 1;
+    int *p;
+    p = &b;
+    if (i==1)
+        *p = 0;
+    return 3 / b;
 }
