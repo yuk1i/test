e53373bb188a638b7bf72313979c5981cc325660
Yuki <chenjf2020@mail.sustech.edu.cn>
2026-01-05 18:49:41 +0800

    Create test_poly.cpp

---
+++ test_poly.cpp
@@ -0,0 +1,28 @@
+
+class Base {
+public:
+    virtual ~Base() = default;
+    int value{};
+    int* ptr{nullptr};
+};
+
+class Derived : public Base {
+public:
+    int **derived_ptr;
+    void run() {
+        this->ptr = &value;
+    }
+};
+
+void sink(int);
+
+int main() {
+    Derived d;
+    d.value = 3;
+    Base* pBase = &d;
+    pBase->ptr = &d.value;
+    d.derived_ptr = &pBase->ptr;
+    **d.derived_ptr = 20;
+    sink(pBase->value);
+    return 0;
+}
